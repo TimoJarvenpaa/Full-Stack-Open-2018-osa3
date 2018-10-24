3f95d9482ce0482f055452bcf7fc75fa64f7067a
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2018-10-24 16:29:54 +0300

    Tehtävä 3.3

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ let persons = [{
   }
 ]
 
-const info = `<p> puhelinluettelossa ${persons.length} henkilön tiedot </p>`
-
-
 app.get('/info', (req, res) => {
   res.send(
     `
@@ -37,6 +34,17 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
