64a8422a6491b2c96431c6bc738a7cb03401273f
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2018-10-27 14:21:10 +0300

    Tehtävä 3.8

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', function (req, res) {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms'))
 
 let persons = [{
     "name": "Arto Hellas",
